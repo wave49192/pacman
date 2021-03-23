60d226aeb6774aeb1dd0d73cea303b50ec8199a0
wave <tumtamai_w@hotmail.com>
2021-03-23 23:36:01 +0700

    Dev 1: Observer pattern (for scoring)

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 
-from gamelib import Sprite, GameApp, Text
-
 from dir_consts import *
+from gamelib import Sprite, GameApp, Text
 from maze import Maze
 
 CANVAS_WIDTH = 800
@@ -11,6 +10,7 @@ CANVAS_HEIGHT = 600
 UPDATE_DELAY = 33
 
 PACMAN_SPEED = 5
+
 
 class Pacman(Sprite):
     def __init__(self, app, maze, r, c):
@@ -21,8 +21,9 @@ class Pacman(Sprite):
         self.direction = DIR_STILL
         self.next_direction = DIR_STILL
 
-        x, y = maze.piece_center(r,c)
+        x, y = maze.piece_center(r, c)
         super().__init__(app, 'images/pacman.png', x, y)
+        self.dot_eaten_observers = []
 
     def update(self):
         if self.maze.is_at_center(self.x, self.y):
@@ -30,7 +31,9 @@ class Pacman(Sprite):
 
             if self.maze.has_dot_at(r, c):
                 self.maze.eat_dot_at(r, c)
-            
+                for i in self.dot_eaten_observers:
+                    i()
+
             if self.maze.is_movable_direction(r, c, self.next_direction):
                 self.direction = self.next_direction
             else:
@@ -55,6 +58,22 @@ class PacmanGame(GameApp):
 
         self.elements.append(self.pacman1)
         self.elements.append(self.pacman2)
+        self.pacman1_score = 0
+        self.pacman2_score = 0
+        self.pacman1.dot_eaten_observers.append(self.dot_eaten_by_pacman1)
+        self.pacman2.dot_eaten_observers.append(self.dot_eaten_by_pacman2)
+
+    def update_scores(self):
+        self.pacman1_score_text.set_text(f'P1: {self.pacman1_score}')
+        self.pacman2_score_text.set_text(f'P2: {self.pacman2_score}')
+
+    def dot_eaten_by_pacman1(self):
+        self.pacman1_score += 1
+        self.update_scores()
+
+    def dot_eaten_by_pacman2(self):
+        self.pacman2_score += 1
+        self.update_scores()
 
     def pre_update(self):
         pass
@@ -81,10 +100,11 @@ class PacmanGame(GameApp):
         elif event.char.upper() == 'L':
             self.pacman2.set_next_direction(DIR_RIGHT)
 
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Monkey Banana Game")
- 
+
     # do not allow window resizing
     root.resizable(False, False)
     app = PacmanGame(root, CANVAS_WIDTH, CANVAS_HEIGHT, UPDATE_DELAY)
