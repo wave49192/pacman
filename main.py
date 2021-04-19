d9a21d40eebfc6e9d78d571f8b149c53d61c40df
wave <tumtamai_w@hotmail.com>
2021-04-19 13:32:57 +0700

    commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tkinter as tk
+import math
 import random
+import tkinter as tk
+from random import randint
+
 from dir_consts import *
 from gamelib import Sprite, GameApp, Text
 from maze import Maze
@@ -10,6 +13,7 @@ CANVAS_HEIGHT = 600
 UPDATE_DELAY = 33
 
 PACMAN_SPEED = 5
+GHOST_SPEED = 4
 
 
 class Pacman(Sprite):
@@ -43,13 +47,65 @@ class Pacman(Sprite):
                 self.direction = self.next_direction
             else:
                 self.direction = DIR_STILL
-        
+
         # we update the location with the new speed variable
         self.state.move_pacman()
 
-
     def set_next_direction(self, direction):
         self.next_direction = direction
+
+
+def vector_len(x, y):
+    return math.sqrt(x * x + y * y)
+
+
+class GhostStrategy:
+    def __init__(self):
+        self.ghost = None
+
+
+class WanderingGhostStrategy(GhostStrategy):
+    def find_next_position(self):
+        if self.ghost != None:
+            new_x = self.ghost.x + randint(-5, 5)
+            new_y = self.ghost.y + randint(-5, 5)
+            return (new_x, new_y)
+        return (0, 0)
+
+
+class KillerGhostStrategy(GhostStrategy):
+    def find_next_position(self):
+        if self.ghost == None:
+            return (0, 0)
+
+        ghost = self.ghost
+        if len(ghost.pacmans) > 0:
+            pacman = ghost.pacmans[0]
+            dx = pacman.x - ghost.x
+            dy = pacman.y - ghost.y
+
+            dlen = vector_len(dx, dy)
+            if dlen > GHOST_SPEED:
+                dx *= GHOST_SPEED / dlen
+                dy *= GHOST_SPEED / dlen
+            new_x = ghost.x + dx
+            new_y = ghost.y + dy
+        else:
+            new_x, new_y = 0, 0
+        return (new_x, new_y)
+
+
+class Ghost(Sprite):
+    def __init__(self, app, image_filename, x, y, ghost_strategy=None):
+        super().__init__(app, image_filename, x, y)
+        self.pacmans = []
+        self.strategy = ghost_strategy
+        self.strategy.ghost = self
+
+    def update(self):
+        new_x, new_y = self.strategy.find_next_position()
+        self.x = new_x
+        self.y = new_y
 
 
 class PacmanGame(GameApp):
@@ -67,18 +123,32 @@ class PacmanGame(GameApp):
 
         self.pacman1_score = 0
         self.pacman2_score = 0
+
         self.pacman1.dot_eaten_observers.append(self.dot_eaten_by_pacman1)
         self.pacman2.dot_eaten_observers.append(self.dot_eaten_by_pacman2)
+        self.ghost1 = Ghost(self, 'images/Ghost.png',
+                            CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2,
+                            KillerGhostStrategy())
+
+        self.ghost2 = Ghost(self, 'images/Ghost.png',
+                            CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2,
+                            WanderingGhostStrategy())
+
+        self.ghost1.pacmans.append(self.pacman1)
+        self.ghost1.pacmans.append(self.pacman2)
+
+        self.elements.append(self.ghost1)
+        self.elements.append(self.ghost2)
 
         self.command_map = {
-            'W' : self.get_pacman_next_direction_function(self.pacman1, DIR_UP),
-            'A' : self.get_pacman_next_direction_function(self.pacman1, DIR_LEFT),
-            'S' : self.get_pacman_next_direction_function(self.pacman1, DIR_DOWN),
-            'D' : self.get_pacman_next_direction_function(self.pacman1,DIR_RIGHT),
-            'I' : self.get_pacman_next_direction_function(self.pacman2, DIR_UP),
-            'J' : self.get_pacman_next_direction_function(self.pacman2, DIR_LEFT),
-            'K' : self.get_pacman_next_direction_function(self.pacman2, DIR_DOWN),
-            'L' : self.get_pacman_next_direction_function(self.pacman2, DIR_RIGHT),
+            'W': self.get_pacman_next_direction_function(self.pacman1, DIR_UP),
+            'A': self.get_pacman_next_direction_function(self.pacman1, DIR_LEFT),
+            'S': self.get_pacman_next_direction_function(self.pacman1, DIR_DOWN),
+            'D': self.get_pacman_next_direction_function(self.pacman1, DIR_RIGHT),
+            'I': self.get_pacman_next_direction_function(self.pacman2, DIR_UP),
+            'J': self.get_pacman_next_direction_function(self.pacman2, DIR_LEFT),
+            'K': self.get_pacman_next_direction_function(self.pacman2, DIR_DOWN),
+            'L': self.get_pacman_next_direction_function(self.pacman2, DIR_RIGHT),
         }
 
     def update_scores(self):
@@ -103,9 +173,8 @@ class PacmanGame(GameApp):
         ch = event.char.upper()
         if ch in self.command_map:
             self.command_map[ch]()
-    
-    def get_pacman_next_direction_function(self, pacman, next_direction):
 
+    def get_pacman_next_direction_function(self, pacman, next_direction):
         def f():
             pacman.set_next_direction(next_direction)
 
@@ -142,10 +211,10 @@ class SuperPacmanState:
         pacman.y += speed * DIR_OFFSET[pacman.direction][1]
 
         self.super_speed_counter += 1
-        
+
         if self.super_speed_counter > 50:
             self.pacman.state = NormalPacmanState(pacman)
-        
+
 
 if __name__ == "__main__":
     root = tk.Tk()
